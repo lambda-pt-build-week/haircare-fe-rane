8a46cba54b6c3aab355c137e8b2d561dd35f4970
ranewallin <github@blasph.me>
2019-04-26 20:26:56 -0700

    setting up grid layout for landing page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const Post = ({ imageURL }) => {
   return (
     <div>
-      <img src={imageURL} alt="image" />
+      <PostImage src={imageURL} alt="image" />
     </div>
   );
 };
 
 export default Post;
+
+const PostImage = styled.img`
+  width: 200px;
+`;
--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 import Post from "./Post";
 import * as keys from "../config/keys";
@@ -28,12 +29,19 @@ class PostContainer extends Component {
 
   render() {
     return (
-      <div>
+      <PostGrid>
         {this.state.images &&
-          this.state.images.map(image => <Post imageURL={image.urls.thumb} />)}
-      </div>
+          this.state.images.map(image => <Post imageURL={image.urls.small} />)}
+      </PostGrid>
     );
   }
 }
 
 export default PostContainer;
+
+const PostGrid = styled.div`
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  border: 2px solid black;
+`;
