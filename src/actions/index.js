b50a64cb35f97adc23f0fc49603b3cefa48180f8
ranewallin <github@blasph.me>
2019-04-30 17:57:33 -0700

    clean up

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,21 +5,15 @@ export const ADD_STYLIST = "ADD_STYLIST";
 export const ADD_SUCCESS = "ADD_SUCCESS";
 export const FAILURE = "FAILURE";
 
-
 export const fetchStylists = () => {
   return dispatch => {
     dispatch({ type: FETCH_STYLISTS });
     axios
-    .get('https://haircare.herokuapp.com/api/stylists')
-    .then(
-      res => dispatch({ type: FETCH_SUCCESS, payload: res.data})
-    )
-    .catch(
-      err => dispatch({ type: FAILURE, payload: err })
-    )
-  }
-}
-
+      .get("https://haircare.herokuapp.com/api/stylists")
+      .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
+      .catch(err => dispatch({ type: FAILURE, payload: err }));
+  };
+};
 
 export const addStylist = newStylist => dispatch => {
   dispatch({ type: ADD_STYLIST });
