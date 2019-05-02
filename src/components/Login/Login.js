e8daf651fa535fabfa6b0ba395d7ce1361a7ff26
cpscott1 <thacam18@gmail.com>
2019-05-01 21:09:49 -0500

    added endpoint to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,36 @@ class Login extends Component {
     password: ""
   }
 
+
+userLogin = event => {
+  event.preventDefault();
+
+  const loginInfo = {
+    username: this.state.username,
+    password: this.state.password
+    }
+
+  axios
+  .post('https://haircare.herokuapp.com/auth/google', loginInfo)
+  .then(res => {
+    localStorage.setItem('jwtToken', res.data.token);
+    localStorage.setItem('user', JSON.stringify(loginInfo))
+
+    const token = localStorage.getItem('jwtToken');
+
+    if(token) {
+      axios.defaults.headers.common['Authorization'] = token;
+    }
+
+    token ? this.props.history.push('/protected')
+    : this.props.history.push('/')
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+
   render() {
     return (
       <div className="login-form">
