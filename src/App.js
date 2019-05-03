f65c9fdf5bda90da9bd90e9dc51d2932c14c2cce
cpscott1 <thacam18@gmail.com>
2019-05-03 13:26:19 -0500

    updated login file

--- src/App.js
+++ src/App.js
@@ -9,33 +9,15 @@ import Stylist from "./components/Stylist/Stylist";
 import Login from "./components/Login/Login";
 import PrivateRoute from "./components/PrivateRoute";
 
-class App extends Component {
-  state = {
-    jwtToken: 'jwtToken'
-  }
-
-componentDidMount() {
-  axios
-  .get('https://haircare.herokuapp.com/auth/google')
-  .then(res => {
-    console.log(this.props.location.pathname)
-     if (this.props.location.pathname != null){
-       localStorage.setItem('jwtToken', res.data)
-       localStorage.getItem(this.state.jwtToken)
-     }
-   })
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: "white" }}>
+      <Route path="/" exact render={props => <Home {...props} />} />
+      <Route path="/post" render={props => <PostDetail {...props} />} />
+      <Route path="/login" component={Login}/>
+      <PrivateRoute exact path="/protected" component={Stylist} />
+    </div>
+  );
 }
 
-  render() {
-    return (
-      <div className="App">
-        <Route path="/" exact render={props => <Home {...props} />} />
-        <Route path="/post" render={props => <PostDetail {...props} />} />
-        <Route path="/login" component={Login}/>
-        <PrivateRoute exact path="/protected" component={Stylist} />
-      </div>
-    );
-  }
-
-}
-export default withRouter(App);
+export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react';
-
+import styled from "styled-components";
 const googleButton = require('../../assets/googlebutton.png');
 
-class Login extends Component {
-  state = {
-    credentials: {
-      username: "",
-      password: ""
-    }
-  }
-
-  loginChangeHandler = event => {
-    this.setState({
-      credentials: {
-        ...this.state.credentials,
-        [event.target.name]: event.target.value
-      }
-    })
-  }
-
-
-userLogin = event => {
-  event.preventDefault();
-  }
-
-
-  render() {
-    return (
-      <div className="login-form">
-        <form onSubmit={this.userLogin}>
-          <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={this.state.credentials.username}
-          onChange={this.loginChangeHandler}
-          />
-          <input
-          type="password"
-          name="password"
-          placeholder="password"
-          value={this.state.credentials.password}
-          onChange={this.loginChangeHandler}
-          />
-          <a href="https://haircare.herokuapp.com/auth/google"><img src={googleButton} alt="google button"/></a>
-        </form>
-      </div>
-    )
-  }
+function Login() {
+  return (
+    <>
+      <a href="https://haircare.herokuapp.com/auth/google">
+        <img src={googleButton} alt="google button"/>
+      </a>
+    </>
+  )
 }
+
 
 export default Login;
