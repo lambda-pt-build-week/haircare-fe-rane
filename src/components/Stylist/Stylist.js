5aa07a9edcd4eaff12ebdf2e86490a96c8ea7110
cpscott1 <thacam18@gmail.com>
2019-05-06 02:42:51 -0500

    adding some styling

--- src/components/Stylist/Stylist.js
+++ src/components/Stylist/Stylist.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { fetchStylists } from '../../actions';
 
-const Stylist = () => {
-  return (
-    <div>Stylist</div>
-  )
+class Stylist extends Component {
+  componentDidMount() {
+   this.props.fetchStylists();
+ }
+
+ render() {
+   const { stylists = [] } = this.props
+   {console.log(stylists)}
+   return (
+     <div>
+      {stylists.map(stylist => {
+        return (
+          <StylistInfo>
+          <StylistImages key={stylist.id} src={stylist.profile_picture} />
+          <div>{stylist.stylist_name}</div>
+          <div>{stylist.location}</div>
+          </StylistInfo>
+
+        )
+      })}
+     </div>
+   )
+ }
+
 }
 
-export default Stylist;
+
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    stylists: state.stylistsReducer.stylists
+  }
+}
+
+export default connect(mapStateToProps, { fetchStylists } )(Stylist);
+
+const StylistInfo = styled.div`
+  `;
+
+const StylistImages = styled.img`
+  height: auto;
+  width: 15%;
+`;
--- src/components/Stylist/StylistContainer.js
+++ src/components/Stylist/StylistContainer.js
@@ -1,11 +1,2 @@
 import React, { Component } from "react";
 import axios from "axios"
-
-class StylistContainer extends Component {
-  }
-  render() {
-
-  }
-}
-
-export default StylistContainer;
