6bbcbed278acb9cd52a8558ec659cc37d236e9ea
ranewallin <github@blasph.me>
2019-05-05 20:52:50 -0700

    add search function

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,24 +1,37 @@
-import { FETCH_POST_STARTED, FETCH_POST_SUCCESS, FETCH_POST_FAILED, GET_POST } from "../actions/postActions";
+import {
+  FETCH_POST_STARTED,
+  FETCH_POST_SUCCESS,
+  FETCH_POST_FAILED,
+  GET_POST,
+  SEARCH_POSTS
+} from "../actions/postActions";
 
 const initialState = {
-    fetchingPosts: false,
-    selectedPost: null,
-    fetchError: "",
-    posts: null
-}
+  fetchingPosts: false,
+  selectedPost: null,
+  fetchError: "",
+  posts: null
+};
 
-export const postReducer = (state=initialState, action) => {
-    switch(action.type) {
-        case FETCH_POST_STARTED:
-            return { ...state, fetchingPosts: true };
-        case FETCH_POST_SUCCESS:
-            return { ...state, posts: action.payload, fetchingPosts: false, fetchError: false };
-        case FETCH_POST_FAILED:
-            return {...state, fetchingPosts: false, fetchError: action.payload }
-        case GET_POST:
-            const selectedPost = state.posts.find(post => post.id === action.payload);
-            return { ...state, selectedPost }
-        default:
-            return state;
-    }
-}
+export const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_POST_STARTED:
+      return { ...state, fetchingPosts: true };
+    case FETCH_POST_SUCCESS:
+      return {
+        ...state,
+        posts: action.payload,
+        fetchingPosts: false,
+        fetchError: false
+      };
+    case FETCH_POST_FAILED:
+      return { ...state, fetchingPosts: false, fetchError: action.payload };
+    case GET_POST:
+      const selectedPost = state.posts.find(post => post.id === action.payload);
+      return { ...state, selectedPost };
+    case SEARCH_POSTS:
+      return { ...state, posts: action.payload };
+    default:
+      return state;
+  }
+};
