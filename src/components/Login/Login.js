a0304dbe50e1c38f2af51b97245452d87774b411
cpscott1 <thacam18@gmail.com>
2019-05-06 20:02:42 -0500

    removed login button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-const googleButton = require('../../assets/googlebutton.png');
-
 function Login() {
   return (
     <>
-      <a href="https://haircare.herokuapp.com/auth/google">
-        <img src={googleButton} alt="google button"/> 
-      </a>
+
     </>
   )
 }
